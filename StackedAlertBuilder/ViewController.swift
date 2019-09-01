9a7460a6dc4c62569e83559f9c90fe311952c95d
Dogan Altinbas <doganaltinbas@hotmail.com>
2019-09-01 18:02:07 +0300

    first commit

--- StackedAlertBuilder/ViewController.swift
+++ StackedAlertBuilder/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("first commit")
         // Do any additional setup after loading the view.
     }
 
