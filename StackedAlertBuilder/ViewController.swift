ebe3393dd12a79a560663e5935058b6a1d28714e
Dogan Altinbas <doganaltinbas@hotmail.com>
2019-09-01 17:59:08 +0300

    Initial Commit

---
+++ StackedAlertBuilder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackedAlertBuilder
+//
+//  Created by Dogan Altinbas on 1.09.2019.
+//  Copyright © 2019 Dogan Altinbas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
