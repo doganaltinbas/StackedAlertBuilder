a21a79e231e3d58557c5e20325f21c92bd925201
Dogan Altinbas <doganaltinbas@hotmail.com>
2019-09-01 23:33:58 +0300

    label with 2 buttons example added

--- StackedAlertBuilder/ViewController.swift
+++ StackedAlertBuilder/ViewController.swift
@@ -12,10 +12,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("first commit")
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showLblAndButton(_ sender: Any) {
+        
+    }
+    @IBAction func showLblAnd2Buttons(_ sender: Any) {
+        let customAlert = self.storyboard?.instantiateViewController(withIdentifier: "CustomAlertID") as! CustomAlertView
+        customAlert.delegate = self
+        let components = AlertBuilder().addLabel(text:"Are you sure you want to quit? All unsaved progress will be lost.").addDoubleButton(firstButtonTitle: "CANCEL", secondButtonTitle: "OK", firstButtonAction: {
+            customAlert.animateOut(afterBlock: { [weak self] in
+                self?.dismiss(animated: true, completion: nil)
+            })
+        }, secondButtonAction: {
+            customAlert.animateOut(afterBlock: { [weak self] in
+                self?.dismiss(animated: true, completion: nil)
+            })
+        }).build()
+        guard let componentArray = components else{
+            return
+        }
+        customAlert.components = componentArray
+        self.present(customAlert, animated: false, completion: nil)
+        self.navigationController?.popViewController(animated: true)
+    }
+    @IBAction func showLblAnd3Buttons(_ sender: Any) {
+        
+    }
+    
+    
 }
 
+
+extension ViewController: CustomAlertViewDelegate {
+    
+    @objc func buttonTapped(action: (() -> ())?) {
+        if let action = action {
+            action()
+        }
+    }
+    
+}
